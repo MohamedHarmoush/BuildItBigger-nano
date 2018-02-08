fc256a03831a804aec9235b6d98098d0e56f68d5
MohamedHarmoush <Mohamedharmoush180@gmail.com>
2018-02-08 10:11:23 +0200

    new commit after edit AsynTask test

--- app/src/test/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/test/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -4,22 +4,30 @@ package com.udacity.gradle.builditbigger;
  * Created by Harmoush on 2/7/2018.
  */
 import android.content.Context;
+import android.util.Log;
 
 import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
+
+import org.junit.Assert;
 import org.mockito.Mock;
 import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
 
 public class EndpointsAsyncTaskTest {
     @Mock
     Context context;
+    String TAG = EndpointsAsyncTask.class.getSimpleName();
     @Test
     public void verifyEndpointsResponse() {
 
         try {
             String joke = new EndpointsAsyncTask().execute(context).get();
            //comment
-            assert (joke!=null);
+            Log.d(TAG,"joke = "+joke);
+            assertNotNull (joke);
             assert(!(joke.equals("fun and fun equal fun!!")));
+            Assert.assertTrue(!joke.equals(""));
         } catch (Exception e) {
             e.fillInStackTrace();
         }
